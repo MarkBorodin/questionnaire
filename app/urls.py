3e7704e363e2b68691f8fdd8442bff2537a84ef6
mark <rens2588@gmail.com>
2021-06-25 16:39:51 +0300

    frontend

--- app/urls.py
+++ app/urls.py
@@ -2,14 +2,8 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
-from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('questionnaire/', include('questionnaire.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-if 'survey' in settings.INSTALLED_APPS:
-    urlpatterns += [
-        url(r'^survey/', include('survey.urls'))
-    ]
