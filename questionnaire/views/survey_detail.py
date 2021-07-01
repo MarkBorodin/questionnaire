cb0fab6d407cf9cc7907498ddcb4a94056b1a680
mark <rens2588@gmail.com>
2021-07-01 11:27:06 +0300

    reverse bug

--- questionnaire/views/survey_detail.py
+++ questionnaire/views/survey_detail.py
@@ -66,7 +66,7 @@ class SurveyDetail(View):
 
         if not survey.editable_answers and form.response is not None:
             LOGGER.info("Redirects to survey list after trying to edit non editable answer.")
-            return redirect(reverse("survey-list"))
+            return redirect(reverse("questionnaire:survey-list"))
         context = {"response_form": form, "survey": survey, "categories": categories}
         if form.is_valid():
             return self.treat_valid_form(form, kwargs, request, survey)
@@ -108,7 +108,7 @@ class SurveyDetail(View):
             return redirect(next_url)
         del request.session[session_key]
         if response is None:
-            return redirect(reverse("survey-list"))
+            return redirect(reverse("questionnaire:survey-list"))
         next_ = request.session.get("next", None)
         if next_ is not None:
             if "next" in request.session:
