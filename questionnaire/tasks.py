567b466544c71b79ff9e01e0da2537a096b2d02a
mark <rens2588@gmail.com>
2021-07-07 11:04:29 +0300

    admins_emails

--- questionnaire/tasks.py
+++ questionnaire/tasks.py
@@ -1,5 +1,6 @@
 from io import StringIO
 
+from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404
 
 from django.core.mail import EmailMessage
@@ -13,6 +14,8 @@ from questionnaire.utils import get_data_for_csv
 
 @shared_task
 def send_an_email_about_the_end_of_the_survey(primary_key):
+    admins = User.objects.filter(is_superuser=True)
+    admins_emails = [admin.email for admin in admins if admin.email]
     survey = get_object_or_404(Survey, pk=primary_key)
     questions_list, answers_list = get_data_for_csv(primary_key)
     csv_file = StringIO()
@@ -24,7 +27,7 @@ def send_an_email_about_the_end_of_the_survey(primary_key):
         subject=f"The client's '{survey.client}' survey results ",
         body=f"""The survey results are in the attached file\nClient: {survey.client}\nTitle: {survey.title}\nName: {survey.name}""", # noqa
         from_email=EMAIL_HOST_USER,
-        to=['rens2588@gmail.com'],
+        to=admins_emails,
     )
     email.attach(f'{survey.name}.csv', csv_file.getvalue(), 'text/csv')
     email.send()
